385ea0f01e1529766a3c27feb4d3ccf82eb2ddbf
Talha Waqar <beastplayz79@gmail.com>
2026-09-20 09:15:56 +0500

    chore: register outreach draft model

--- alembic/env.py
+++ alembic/env.py
@@ -3,7 +3,7 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 import os
 from src.models.base import Base
-from src.models.core import Project, Contractor, MatchRecord, MatchReviewAudit
+from src.models.core import Project, Contractor, MatchRecord, MatchReviewAudit, OutreachDraft
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
